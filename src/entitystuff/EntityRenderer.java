5a9460f595a19695f987fef067316b6cb1eeb4e3
nate0927 <nathanielawelch@gmail.com>
2014-09-30 18:23:48 -0400

    Working on entity renderers.

---
+++ src/entitystuff/EntityRenderer.java
@@ -0,0 +1,18 @@
+package entitystuff;
+
+import java.util.HashMap;
+
+import org.newdawn.slick.SpriteSheet;
+
+public class EntityRenderer {
+	private HashMap<String, SpriteSheet> spriteSheets;
+	private HashMap<Integer, Animation> animations;
+	public EntityRenderer(int x, int y){
+		animations = new HashMap<Integer, Animation>();
+	}
+	
+	
+	public void cacheSpriteSheet(String filename){
+		
+	}
+}
